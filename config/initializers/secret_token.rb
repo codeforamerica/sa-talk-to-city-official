0fd865e20c9ca5194a08103986de2e9d9f5294a8
Amy Mok <amymok@codeforamerica.org>
2014-04-15 17:24:30 -0700

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TalkToCityOfficial::Application.config.secret_key_base = '9d95602dade6f8e7dc4d7e83701a7de5f016dd70d991019633704b5c4d059508d694257eabe717b7330f3e0d681680b06677ca9f31e0382f3d34e0d81d91e241'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TalkToCityOfficial::Application.config.session_store :cookie_store, key: '_talk-to-city-official_session'
