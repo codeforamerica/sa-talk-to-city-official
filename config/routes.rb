bbf67efb602b36683ca57c01023a6b02b0493902
Amy Mok <amymok@codeforamerica.org>
2014-04-15 22:42:35 -0700

    add twilio

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ TalkToCityOfficial::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  post 'twilio/voice' => 'twilio#voice'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
